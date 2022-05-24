f6f8e4c27466d5425dc1eb5b666ff787f1272f07
pantherinblack <stupark@bzz.ch>
2022-05-24 22:31:02 +0200

    Fix comments in User class

--- src/main/java/ch/bzz/applicationmanager/module/User.java
+++ src/main/java/ch/bzz/applicationmanager/module/User.java
@@ -13,34 +13,74 @@ public class User {
     private String password;
     private String userRole;
 
+    /**
+     * gets the user uuid.
+     *
+     * @return uuid
+     */
     public String getUserUUID() {
         return userUUID;
     }
 
+    /**
+     * sets the user uuid.
+     *
+     * @param userUUID
+     */
     public void setUserUUID(String userUUID) {
         this.userUUID = userUUID;
     }
 
+    /**
+     * gets the user name.
+     *
+     * @return name
+     */
     public String getUserName() {
         return userName;
     }
 
+    /**
+     * sets the user name.
+     *
+     * @param userName
+     */
     public void setUserName(String userName) {
         this.userName = userName;
     }
 
+    /**
+     * gets the user password.
+     *
+     * @return password
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * sets the user password.
+     *
+     * @param password
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 
+    /**
+     * gets the user role.
+     *
+     * @return role
+     */
     public String getUserRole() {
         return userRole;
     }
 
+    /**
+     * sets the user role.
+     *
+     * @param userRole
+     */
     public void setUserRole(String userRole) {
         this.userRole = userRole;
     }
